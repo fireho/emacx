2537acc507e3e1703d264daf2a520c5a3fe3e500
Marcos Piccinini <x@nofxx.com>
2015-12-18 01:01:10 -0200

    elpa up

--- .emacs.d/elpa/rubocop-20150630.1003/rubocop-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "rubocop" "20150630.1003" "An Emacs interface for RuboCop" '((dash "1.0.0") (emacs "24")) :url "https://github.com/bbatsov/rubocop-emacs" :keywords '("project" "convenience"))
