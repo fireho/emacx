f5fddc681779dca4a099178f18294a3cc234e25c
nofxx <chadart@gmail.com>
2025-04-14 23:43:24 -0300

    new stuff

---
+++ .emacs.d/elpa/elixir-mode-20230626.1738/elixir-mode-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "elixir-mode" "20230626.1738"
+  "Major mode for editing Elixir files."
+  '((emacs "25"))
+  :url "https://github.com/elixir-editors/emacs-elixir"
+  :commit "00d6580a040a750e019218f9392cf9a4c2dac23a"
+  :revdesc "00d6580a040a"
+  :keywords '("languages" "elixir"))
---
+++ .emacs.d/elpa/gptel-20250412.1632/gptel-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "gptel" "20250412.1632"
+  "Interact with ChatGPT or other LLMs."
+  '((emacs     "27.1")
+    (transient "0.7.4")
+    (compat    "29.1.4.1"))
+  :url "https://github.com/karthink/gptel"
+  :commit "8329ee709ebf91d59a07dc13f193f173118b1ae2"
+  :revdesc "8329ee709ebf"
+  :keywords '("convenience" "tools")
+  :authors '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com"))
+  :maintainers '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com")))
